a58f27440b93a0f74aa1222314c39bf4360cb73c
lautaro DSB <ldsb18@gmail.com>
2022-09-06 18:04:50 -0300

    More practicing with states

--- part1/practice/src/App.js
+++ part1/practice/src/App.js
@@ -1,3 +1,4 @@
+import { click } from '@testing-library/user-event/dist/click'
 import { useState } from 'react'
 
 const Hello = ({name, age}) => {
@@ -16,14 +17,55 @@ const Hello = ({name, age}) => {
 	)
 }
 
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+	<button onClick={onClick}>
+		{text}
+	</button>
+)
+
+const History = (props) => {
+	if (props.allClicks.length === 0){
+		return (
+			<div>
+				the app is used by pressing the buttons
+			</div>
+		)
+	}
+	
+	return(
+		<div>
+			button press history: {props.allClicks.join(' ')}
+		</div>
+	)
+}
+
 const App = () => {
 	const name = 'Peter'
 	const age = 10
 
 	const [ counter, setCounter ] = useState(0)
 
+	const [ clicks, setClicks ] = useState({
+		left: 0, right: 0
+	})
+
+	const [ allClicks , setAll] = useState([])
+
 	const increaseByOne = () => setCounter(counter + 1)
+	const decreaseByOne = () => setCounter(counter - 1)
 	const setToZero = () => setCounter(0)
+
+	const handleLeftClick = () => {
+		setAll(allClicks.concat('L'))
+		setClicks({ ...clicks, left: clicks.left + 1 })
+	}
+
+	const handleRightClick = () => {
+		setAll(allClicks.concat('R'))
+		setClicks({ ...clicks, right: clicks.right + 1 })
+	}
 
 	return(
 		<div>
@@ -35,16 +77,22 @@ const App = () => {
 			</div>
 
 			<div>
-				{counter}
+				<Display counter={counter} />
+				<Button onClick={increaseByOne} text="plus" />
+				<Button onClick={decreaseByOne} text="minus" />
+				<Button onClick={setToZero} text="zero" />
 			</div>
 
-			<button onClick={increaseByOne}>
-				plus
-			</button>
-			<button onClick={setToZero}>
-				zero
-			</button>
+			<br/>
 
+			<div>
+				{clicks.left}
+				<Button onClick={handleLeftClick} text="Left"/>
+				<Button onClick={handleRightClick} text="Right"/>
+				{clicks.right}
+
+				<History allClicks={allClicks} />
+			</div>
 		</div>
 	)
 }

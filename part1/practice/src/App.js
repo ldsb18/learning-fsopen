dbefb3d53e3537950dbfda014e6028374cbaf357
lautaro DSB <ldsb18@gmail.com>
2022-09-06 17:07:28 -0300

    Practice some state handling functionality

--- part1/practice/src/App.js
+++ part1/practice/src/App.js
@@ -1,8 +1,16 @@
-const Hello = (props) => {
+import { useState } from 'react'
+
+const Hello = ({name, age}) => {
+
+	const bornYear = () => new Date().getFullYear() - age
+
 	return(  
 		<div>
 			<p>
-				Hello {props.name}, you are {props.age} years old
+				Hello {name}, you are {age} years old
+			</p>
+			<p>
+				So you were probably born in {bornYear()}
 			</p>
 		</div>
 	)
@@ -12,11 +20,31 @@ const App = () => {
 	const name = 'Peter'
 	const age = 10
 
+	const [ counter, setCounter ] = useState(0)
+
+	const increaseByOne = () => setCounter(counter + 1)
+	const setToZero = () => setCounter(0)
+
 	return(
 		<div>
-			<h1>Greetings</h1>
-			<Hello name="Maya" age={26 + 10}/>
-			<Hello name={name} age={age}/>
+
+			<div>
+				<h1>Greetings</h1>
+				<Hello name="Maya" age={26 + 10}/>
+				<Hello name={name} age={age}/>
+			</div>
+
+			<div>
+				{counter}
+			</div>
+
+			<button onClick={increaseByOne}>
+				plus
+			</button>
+			<button onClick={setToZero}>
+				zero
+			</button>
+
 		</div>
 	)
 }

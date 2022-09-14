69af5519cbd13b5ea2984470ee0fc296ce97ab4e
lautaro DSB <ldsb18@gmail.com>
2022-09-14 19:58:47 -0300

    Finished mongoDB practice

---
+++ part3/practice/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+   console.log('Please provide the password as an argument: node mongo.js <password>'); 
+   process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://Vaskyat:${password}@cluster0.ecqft7p.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+const noteSchema = new mongoose.Schema({
+	content: String,
+	date: Date,
+	important: Boolean,
+});
+
+const Note = mongoose.model('Note', noteSchema);
+
+mongoose
+	.connect(url)
+	.then( (result) => {
+		console.log('connected');
+
+		Note.find({}).then( resp => {
+			resp.forEach(note => {
+				console.log(note);
+			})
+			mongoose.connection.close();
+		})
+	})
+
+
+	   /* const note = new Note({
+			content: 'Callback-functions suck',
+			date: new Date(),
+			important: true,
+		});
+
+		return note.save();
+	})
+	.then( () => {
+		console.log('note saved');
+
+		return mongoose.connection.close();
+	})
+	.catch( (error) => console.log(error))*/

a6931634c2dd3b61004504c17e49bd23d2e7939e
lautaro DSB <ldsb18@gmail.com>
2022-09-16 17:35:59 -0300

    Done some practice, added validation to note model

--- part3/practice/index.js
+++ part3/practice/index.js
@@ -51,10 +51,12 @@ const unknownEndpoint = (request, response) => {
 }
 
 const errorHandler = (error, request, response, next) => {
-	console.log(error.nessage);
+	console.log(error.message, error.name);
 
-	if (error.name = 'CastError') {
+	if (error.name === 'CastError') {
 		return response.status(400).send({ error: 'malformatted id'});
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message });
 	}
 
 	next(error);
@@ -94,7 +96,7 @@ app.delete('/api/notes/:id', (request, response) => {
 		.catch( err => next(err))	
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
 
 	const body = request.body;
 
@@ -110,20 +112,20 @@ app.post('/api/notes', (request, response) => {
 		date: new Date(),
 	})
 
-	note.save().then( savedNote => {
-		response.json(savedNote)
-	})
+	note.save()
+		.then( savedNote => {
+			response.json(savedNote)
+		})
+		.catch( err => next(err))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-	const body = request.body;
+	const { content, important } = request.body;
 
-	const note = {
-		content: body.content,
-		important: body.important,
-	}
-
-	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+	Note.findByIdAndUpdate(
+		request.params.id, 
+		{ content, important }, 
+		{ new: true, runValidators: true, context: 'query' })
 		.then(updatedNote => {
 			response.json(updatedNote);
 		})

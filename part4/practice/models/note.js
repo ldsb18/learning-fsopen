f1cea561c908e98d8d50ff7df278c3f712de49f6
lautaro DSB <ldsb18@gmail.com>
2022-09-17 11:45:04 -0300

    Done some practice, initialiced project structure for exercise 4.1

---
+++ part4/practice/models/note.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const noteSchema = mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+        minLength: 5
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)

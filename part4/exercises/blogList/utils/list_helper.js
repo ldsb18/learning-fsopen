6a12c890bb77eb4188e5399688bbd943175c50a6
lautaro DSB <ldsb18@gmail.com>
2022-09-18 14:56:25 -0300

    Finished exercise 4.4 - helper functions and unit tests step2

--- part4/exercises/blogList/utils/list_helper.js
+++ part4/exercises/blogList/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    return blogs === []
+        ? 0
+        : blogs.reduce( (sum, blog) => {
+            return blog.likes + sum
+        }, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }

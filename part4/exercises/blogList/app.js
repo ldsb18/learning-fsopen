340c50deb006df5b05819e1a4e0aac136f86e1b4
lautaro DSB <ldsb18@gmail.com>
2022-09-18 10:05:05 -0300

    Finished exercises 4.1 and 4.2 - Blog list steps 1&2

---
+++ part4/exercises/blogList/app.js
@@ -0,0 +1,41 @@
+const config = require('./utils/config')
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const blogRouter = require ('./controllers/blogs')
+
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+
+const mongoose = require('mongoose')
+
+logger.info(`Connecting to: `, config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)  
+	.then( result => {
+		logger.info(`Connected to MongoDB`);
+	})
+	.catch( err => {
+		logger.error(`Failed to connect to MongoDB`, err.message)
+	})
+
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+
+//Middleware for loggin on console all requests
+app.use(middleware.requestLogger)
+
+
+
+//The app router, handle request for route 'api/blogs'
+app.use('/api/blogs', blogRouter)
+
+//Middleware for handling bad URLs GET and handling error, respectively
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
---
+++ part4/exercises/blogList/controllers/blogs.js
@@ -0,0 +1,50 @@
+const blogRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+
+blogRouter.get('/', (request, response) => {
+	Blog.find({})
+		.then( blogs => {
+			response.json(blogs)
+		})
+})
+
+blogRouter.get('/:id', (request, response, next) => {
+	Blog.findById(request.params.id)
+		.then( blog => {
+			if (blog) {
+				response.json(blog)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch( err => next(err))
+})
+
+blogRouter.post('/', (request, response, next) => {
+	
+	const body = request.body;
+
+	const newBlog = new Blog({
+		title: body.title,
+		author: body.author,
+		url: body.url,
+		likes: body.likes
+	})
+	
+	newBlog.save()
+		.then( savedNote => {
+			response.status(201).json(savedNote)
+		})
+		.catch( err => next(err))
+})
+
+blogRouter.delete('/:id', (request, response, next) => {
+	Blog.findByIdAndDelete(request.params.id)
+		.then( result => {
+			response.status(204).end();
+		})
+		.catch( err => next(err))
+})
+
+module.exports = blogRouter

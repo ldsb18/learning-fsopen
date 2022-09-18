f7dec1251c07a9ef07eafef44c668076d0e0fdfb
lautaro DSB <ldsb18@gmail.com>
2022-09-18 15:30:13 -0300

    Finished exercise 4.5 - helper functions and unit tests step3

--- part4/exercises/blogList/utils/list_helper.js
+++ part4/exercises/blogList/utils/list_helper.js
@@ -1,16 +1,39 @@
 const dummy = (blogs) => {
-    return 1
+	return 1
 }
 
 const totalLikes = (blogs) => {
-    return blogs === []
-        ? 0
-        : blogs.reduce( (sum, blog) => {
-            return blog.likes + sum
-        }, 0)
+	return (
+		blogs.reduce( (sum, blog) => {
+			return blog.likes + sum
+		}, 0)
+	)
+}
+
+const favoriteBlog = (blogs) => {
+	
+	const max = {
+		title: '',
+		author: '',
+		likes: 0
+	}
+
+	
+	blogs.forEach(blog => {
+		if (blog.likes > max.likes) {
+			max.likes = blog.likes
+			max.author = blog.author
+			max.title = blog.title
+		}
+	})
+
+	return blogs.length === 0
+		?	null
+		:	max
 }
 
 module.exports = {
-    dummy,
-    totalLikes
+	dummy,
+	totalLikes,
+	favoriteBlog
 }

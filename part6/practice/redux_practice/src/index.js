54c0ce0977967a6f67dc5c8efc20ee2002e5bda9
Vaskyat <ldsb18@gmail.com>
2023-06-08 02:59:31 -0300

    Initialices redux notes practice

--- part6/practice/redux_practice/src/index.js
+++ part6/practice/redux_practice/src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { createStore } from 'redux'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const counterReducer = (state = 0, action) => {
+	
+	switch (action.type) {
+		case 'INCREMENT':
+			console.log(action.type);
+			return state + 1;
+
+		case 'DECREMENT':
+			console.log(action.type);
+			return state - 1;
+			
+		case 'ZERO':
+			console.log(action.type);
+			return 0;
+
+		default: //if none of the above matches, code comes here
+			return state
+	}
+}
+
+const store = createStore(counterReducer)
+
+const CustomButton = ({ buttonName, action }) => {
+	
+	const handler = () => {
+		store.dispatch(action)
+	}
+
+	return(
+		<>
+			<button onClick={() => handler() }>
+				{ buttonName }
+			</button>
+		</>
+	)
+}
+
+const App = () => {
+
+	return(
+		<div>
+			<h1>Title</h1>
+			
+			<div>
+				{ store.getState() }
+			</div>
+
+			<CustomButton buttonName="plus" action={ {type: 'INCREMENT'} } />
+			<CustomButton buttonName="minus" action={ {type: 'DECREMENT'} } />
+			<CustomButton buttonName="zero" action={ {type: 'ZERO'} } />
+
+		</div>
+	)
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+	root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)

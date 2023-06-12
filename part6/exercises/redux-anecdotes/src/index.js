44aa8084a64a1e575784e8486e30a7233b4a8f74
Vaskyat <ldsb18@gmail.com>
2023-06-11 23:27:53 -0300

    finished exercise 6.3

--- part6/exercises/redux-anecdotes/src/index.js
+++ part6/exercises/redux-anecdotes/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import App from './App'
 import reducer from './reducers/anecdoteReducer'
 
 const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+	<Provider store={store}>
+		<App />
+	</Provider>
 )
